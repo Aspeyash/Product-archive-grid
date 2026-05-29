d8cd04742b0c5482d9e57927f7648b4dab2c7cb7
Aspeyash <166848454+Aspeyash@users.noreply.github.com>
2026-05-30 06:41:12 +0800

    product-archive-grid.php

--- product-archive-grid.php
+++ product-archive-grid.php
@@ -6,7 +6,7 @@
  * Version:           1.0.2
  * Requires at least: 6.0
  * Requires PHP:      7.4
- * Author:            Your Name
+ * Author:            ZYMARG
  * Author URI:        https://example.com
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
